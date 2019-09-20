b6e33ec94d10695b66fcafcbbed81eabbbf9b75d
“g_usergit config user.name “l_user <masamasa0669@gmail.com>
2019-09-20 18:14:55 +0900

    create post,edit,delete function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190920060007) do
+ActiveRecord::Schema.define(version: 20190920062820) do
+
+  create_table "blogs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "title",      limit: 65535, null: false
+    t.text     "content",    limit: 65535, null: false
+    t.text     "image",      limit: 65535
+    t.integer  "user_id",                  null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
@@ -26,4 +36,5 @@ ActiveRecord::Schema.define(version: 20190920060007) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "blogs", "users"
 end
